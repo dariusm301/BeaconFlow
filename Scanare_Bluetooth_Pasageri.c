cec28ab16c2510ff80ac9fde8fe568dbe00ca9d2
Silviuh864 <140816135+Silviuh864@users.noreply.github.com>
2026-02-10 13:51:06 +0200

    Scanare bilete
    Am adaugat asupra codului de netezire a rssi, scanare dupa UUID, pentru a putea distinge dispozitivele care ne priveaza pe noi. De asemenea, am implementat logica pentru prelucrarea datelor din Manufacturer Data, vom avea stocat Id-ul de pe biletul pasagerilor.

---
+++ Scanare_Bluetooth_Pasageri.c
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h> 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+#include "esp_bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_bt_main.h"
+static const uint8_t TARGET_SERVICE_UUID[16] = {
+    0x9a, 0xd1, 0x0a, 0xdb, 0x80, 0x45, 0x12, 0xa4, 
+    0xec, 0x4b, 0x7d, 0x4f, 0x7a, 0x19, 0x62, 0x80
+};
+#define MAX_PASSENGERS 100
+char ticket[20];
+#define PATH_LOSS_INDEX     2.5f    
+#define PROCESS_NOISE       0.015f
+#define CALIBRATION_SAMPLES 100
+float measurement_noise_R = 5.0f;
+bool is_calibrated = false;          
+float dynamic_rssi_1m = -59.0f;       
+float calibration_sum = 0.0f;
+float calibration_sq_sum = 0.0f;        
+int calibration_count = 0;            
+
+
+typedef struct {
+    float Q;           
+    float R;           
+    float x_est_last;  
+    float P_last;      
+} KalmanFilter;
+typedef struct {
+    char ticket_id[20];
+    KalmanFilter kf;
+    uint32_t last_seen;
+    bool is_active;
+} Passenger;
+
+Passenger passengers[MAX_PASSENGERS];
+
+
+KalmanFilter kf;
+
+
+void Kalman_Init(KalmanFilter* k, float process_noise, float measurement_noise, float initial_value) {
+    k->Q = process_noise;
+    k->R = measurement_noise;
+    k->x_est_last = initial_value;
+    k->P_last = 1.0f; 
+}
+
+
+float Kalman_Update(KalmanFilter* k, float raw_val) {
+   
+    float P_temp = k->P_last + k->Q; 
+    float K = P_temp / (P_temp + k->R);
+    float x_est = k->x_est_last + K * (raw_val - k->x_est_last);
+    float P = (1 - K) * P_temp;
+    k->P_last = P;
+    k->x_est_last = x_est;    
+    return x_est;
+}
+
+
+
+
+bool adv_data_find_UUID(uint8_t *adv_data, uint8_t adv_data_len, const uint8_t *TARGET_SERVICE_UUID,char *out_ticket_id) {
+    uint8_t *p_data = adv_data;
+    uint8_t data_len = adv_data_len;
+    bool uuid_found = false;
+    bool ticket_id_found = false;
+     uint8_t id_len =0;
+    while (data_len > 0) {
+        uint8_t field_len = *p_data++;
+        data_len--;
+        if (field_len == 0 || data_len < field_len) break;
+        uint8_t field_type = *p_data++;
+        data_len--;
+        
+        if (field_type == 0x06 || field_type == 0x07) { 
+            uint8_t uuid_len = field_len - 1;
+            if (uuid_len ==16 && memcmp(p_data, TARGET_SERVICE_UUID,16) == 0) {
+                uuid_found=true;
+            }
+        }
+        else if(field_type==0xFF){
+            if(field_len>3)
+            id_len = field_len - 3;
+            memcpy(out_ticket_id, p_data + 2, id_len);
+            out_ticket_id[id_len] = '\0';
+            ticket_id_found = true;
+
+        }
+        p_data += (field_len - 1);
+        data_len -= (field_len - 1);
+    }
+    return (uuid_found && ticket_id_found);
+}
+
+static esp_ble_scan_params_t ble_scan_params = {
+    .scan_type = BLE_SCAN_TYPE_ACTIVE,
+    .own_addr_type = BLE_ADDR_TYPE_PUBLIC,
+    .scan_filter_policy = BLE_SCAN_FILTER_ALLOW_ALL,
+    .scan_interval = 0x50, 
+    .scan_window = 0x50,  
+    .scan_duplicate = BLE_SCAN_DUPLICATE_DISABLE 
+};
+
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) {
+    
+    
+    if (event == ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT) {
+        esp_ble_gap_start_scanning(0);
+    } 
+    
+    
+    else if (event == ESP_GAP_BLE_SCAN_RESULT_EVT) {
+        esp_ble_gap_cb_param_t *scan = (esp_ble_gap_cb_param_t *)param;
+        
+        if (scan->scan_rst.search_evt == ESP_GAP_SEARCH_INQ_RES_EVT) {
+            
+            char current_ticket[20];
+            if (adv_data_find_UUID(scan->scan_rst.ble_adv, scan->scan_rst.adv_data_len, TARGET_SERVICE_UUID,current_ticket)) {
+    
+    int raw_rssi = scan->scan_rst.rssi;
+    int index=-1;
+    
+    if (!is_calibrated) {
+        calibration_sum += raw_rssi;
+        calibration_sq_sum += (raw_rssi * raw_rssi);
+        calibration_count++;
+        
+        printf("CALIBRARE... %d/%d (Raw RSSI: %d)\n", calibration_count, CALIBRATION_SAMPLES, raw_rssi);
+        
+        if (calibration_count >= CALIBRATION_SAMPLES) {
+            float mean_rssi = calibration_sum / (float)CALIBRATION_SAMPLES;
+            dynamic_rssi_1m = mean_rssi;
+            float variance = (calibration_sq_sum / (float)CALIBRATION_SAMPLES) - (mean_rssi * mean_rssi);
+            if (variance < 0.1f) variance = 0.1f; 
+            measurement_noise_R = variance;
+            
+            
+            Kalman_Init(&kf, PROCESS_NOISE, measurement_noise_R, dynamic_rssi_1m);
+            
+            is_calibrated = true;
+            
+            printf("\n--- CALIBRARE MEDIU COMPLETA ---\n");
+            printf("Referinta (Mean) la 1m: %.2f dBm\n", dynamic_rssi_1m);
+            printf("Zgomot Masurat (R): %.4f (Varianta calculata)\n", measurement_noise_R);
+            printf("------------------------------\n\n");
+        }
+    } 
+    else {
+        for (int i = 0; i < MAX_PASSENGERS; i++) {
+        if (passengers[i].is_active && strcmp(passengers[i].ticket_id, current_ticket) == 0) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == -1) {
+        for (int i = 0; i < MAX_PASSENGERS; i++) {
+            if (!passengers[i].is_active) {
+                index = i;
+                strcpy(passengers[index].ticket_id, current_ticket);
+                Kalman_Init(&passengers[index].kf, PROCESS_NOISE, measurement_noise_R, (float)raw_rssi);
+                passengers[index].is_active = true;
+                break;
+            }
+        }
+    }
+
+    if (index != -1) {
+        float filtered_rssi = Kalman_Update(&passengers[index].kf, (float)raw_rssi);
+        float ratio = (dynamic_rssi_1m - filtered_rssi) / (10.0f * PATH_LOSS_INDEX);
+        float dist_m = pow(10.0f, ratio);
+        
+        printf("Bilet: %s | RSSI: %d | Distanta: %.2f m\n", passengers[index].ticket_id, raw_rssi, dist_m);
+    }
+}
+    }
+}
+}
+    }
+
+void app_main(void) {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+
+    Kalman_Init(&kf, PROCESS_NOISE, measurement_noise_R, dynamic_rssi_1m);
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    esp_bt_controller_init(&bt_cfg);
+    esp_bt_controller_enable(ESP_BT_MODE_BLE);
+    esp_bluedroid_init();
+    esp_bluedroid_enable();
+    esp_ble_gap_register_callback(esp_gap_cb);
+    esp_ble_gap_set_scan_params(&ble_scan_params);
+
+    printf("--- SYSTEM READY ---\n");
+    printf("Legend: Raw_RSSI, Filtered_RSSI, Distance(m)\n");
+}
