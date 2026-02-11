ebebaccb6ab3181f8b964177bddf611845031bb4
Darius-30 <HDarius-Marian@proton.me>
2026-02-11 14:12:00 +0200

    First push of the app - can set UUID, ManufactureID and advertise them

---
+++ android/app/src/main/java/com/beaconflow/app/BluetoothManagerPlugin.java
@@ -0,0 +1,123 @@
+package com.beaconflow.app;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.util.Log;
+import android.os.ParcelUuid;
+import java.util.UUID;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.PermissionState;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.annotation.Permission;
+import com.getcapacitor.annotation.PermissionCallback;
+
+
+@CapacitorPlugin(
+    name = "BluetoothManager",
+    permissions = {
+        @Permission(
+            alias = "bluetooth",
+            strings = {
+                android.Manifest.permission.BLUETOOTH_ADVERTISE,
+                android.Manifest.permission.BLUETOOTH_CONNECT
+            }
+        )
+    }
+)
+public class BluetoothManagerPlugin extends Plugin {
+
+    private BluetoothLeAdvertiser globalAdvertiser;
+    private AdvertiseCallback advertiseCallback;
+
+    @PluginMethod
+    public void startAdvertise(PluginCall call) {
+        if (getPermissionState("bluetooth") != PermissionState.GRANTED) {
+            requestPermissionForAlias("bluetooth", call, "checkPermissionsCallback");
+            return;
+        }
+        actualStartAdvertising(call);
+    }
+
+    @PermissionCallback
+    private void checkPermissionsCallback(PluginCall call) {
+        if (getPermissionState("bluetooth") == PermissionState.GRANTED) {
+            actualStartAdvertising(call);
+        } else {
+            call.reject("Permisiuni refuzate de utilizator.");
+        }
+    }
+
+    private void actualStartAdvertising(PluginCall call) {
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+
+        globalAdvertiser = adapter.getBluetoothLeAdvertiser();
+        if (globalAdvertiser == null) {
+            call.reject("Bluetooth-ul este oprit.");
+            return;
+        }
+
+        if (advertiseCallback != null) {
+            globalAdvertiser.stopAdvertising(advertiseCallback);
+        }
+
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
+                .setConnectable(false)
+                .build();
+
+        String mIdString = call.getString("manufacturerId");
+        String uuidString = call.getString("uuid");
+        int mId;
+        try {
+            mId = Integer.parseInt(mIdString, 16);
+        } catch (NumberFormatException e) {
+            mId = 0xFFFF;
+        }
+
+        AdvertiseData data = new AdvertiseData.Builder()
+                .setIncludeDeviceName(false)
+                .addManufacturerData(mId, new byte[0])
+                .addServiceUuid(new ParcelUuid(UUID.fromString(uuidString))) 
+                .build();
+
+        advertiseCallback = new AdvertiseCallback() {
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                super.onStartSuccess(settingsInEffect);
+                JSObject ret = new JSObject();
+                ret.put("status", "advertising");
+                call.resolve(ret);
+            }
+
+            @Override
+            public void onStartFailure(int errorCode) {
+                super.onStartFailure(errorCode);
+                call.reject("Eroare pornire: " + errorCode);
+            }
+        };
+
+        globalAdvertiser.startAdvertising(settings, data, advertiseCallback);
+    }
+
+    @PluginMethod
+    public void stopAdvertise(PluginCall call) {
+        if (globalAdvertiser != null && advertiseCallback != null) {
+            globalAdvertiser.stopAdvertising(advertiseCallback);
+            globalAdvertiser = null;
+            advertiseCallback = null;
+            JSObject ret = new JSObject();
+            ret.put("status", "stopped");
+            call.resolve(ret);
+        } else {
+            call.resolve();
+        }
+    }
+}
---
+++ src/app/app.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { registerPlugin } from '@capacitor/core';
+
+interface BluetoothPlugin {
+  startAdvertise(options: {uuid: string, manufacturerId: string }): Promise<{ status: string }>;
+  stopAdvertise(): Promise<{ status: string }>;
+}
+
+const BluetoothManager = registerPlugin<BluetoothPlugin>('BluetoothManager');
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  uuid: string = '736bc65c-ac3e-4de8-8f41-a713a9a80ad6';
+  manufacturerId: string = 'FFFF';
+  statusMessage: string = 'Status: Ready';
+
+  async startBle() {
+    try {
+      this.statusMessage = 'Status: Advertising';
+      const result = await BluetoothManager.startAdvertise({
+        uuid: this.uuid,
+        manufacturerId: this.manufacturerId
+      });
+    } catch (e: any) {
+      this.statusMessage = 'Error: ' + e.message;
+    }
+  }
+
+  async stopBle() {
+    try {
+      this.statusMessage = 'Status: Stopped';
+      const result = await BluetoothManager.stopAdvertise();
+    } catch (e: any) {
+      this.statusMessage = 'Stop Error: ' + e.message;
+    }
+  }
+}
